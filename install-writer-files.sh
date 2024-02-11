55ecba0cc6b15e5d3e51db60660a71a87d786557
Excel <excel@play.runicrealms.com>
2024-02-11 14:39:36 -0500

    Updated to process writer-files target from writer-files-config

--- install-writer-files.sh
+++ install-writer-files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if all arguments are provided
-if [ "$#" -ne 8 ]; then
-    echo "Usage: $0 <GitHub Path> <Branch> <GitHub Username> <GitHub PAT> <Local Copy Base> <Config GitHub Path> <Config GitHub Branch> <Config GitHub Filepath>"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 <GitHub Username> <GitHub PAT> <Local Copy Base> <Config GitHub Path> <Config GitHub Branch> <Config GitHub Filepath>"
     exit 1
 fi
 
@@ -10,29 +10,34 @@ echo
 echo "Downloading Writer-Files from GitHub"
 
 # Variables
-GITHUB_PATH="$1"
-BRANCH="$2"
-USERNAME="$3"
-PAT="$4"
-COPY_BASE_PATH="$5"
-CONFIG_GITHUB_PATH="$6"
-CONFIG_GITHUB_BRANCH="$7"
-CONFIG_GITHUB_FILEPATH="$8"
+USERNAME="$1"
+PAT="$2"
+COPY_BASE_PATH="$3"
+CONFIG_GITHUB_PATH="$4"
+CONFIG_GITHUB_BRANCH="$5"
+CONFIG_GITHUB_FILEPATH="$6"
 
 # Create temp dir
 TEMP_DIR=$(mktemp -d)
 
-# Clone the repository using credentials
-CLONE_URL=$(echo "https://github.com/$GITHUB_PATH" | sed "s|://|://${USERNAME}:${PAT}@|")
-git clone -b "$BRANCH" "$CLONE_URL" "$TEMP_DIR/writer" --depth 1 --quiet || { echo "Failed to clone repository"; exit 1; }
-
-echo "Processing writer files (unzipping, copying files, etc)"
-
 # Download the file mappings from the config
-CONFIG_TARGET="$TEMP_DIR/writer/${CONFIG_GITHUB_FILEPATH}"
+CONFIG_TARGET="$TEMP_DIR/${CONFIG_GITHUB_FILEPATH}"
 CONFIG_URL="https://raw.githubusercontent.com/${CONFIG_GITHUB_PATH}/${CONFIG_GITHUB_BRANCH}/${CONFIG_GITHUB_FILEPATH}"
-echo "Downloading file mappings from $CONFIG_URL to local $CONFIG_TARGET"
+echo "Downloading file mappings $CONFIG_GITHUB_FILEPATH from $CONFIG_GITHUB_PATH:$CONFIG_GITHUB_BRANCH to local"
 curl -sL -H "Authorization: token ${PAT}" "$CONFIG_URL" --output "$CONFIG_TARGET"
+
+# Parse file mappings for writer-files target
+GITHUB_PATH=$(yq e '.repo.path' "$CONFIG_TARGET")
+GITHUB_BRANCH=$(yq e '.repo.branch' "$CONFIG_TARGET")
+echo "Found writer-files targets at $GITHUB_PATH:$GITHUB_BRANCH"
+
+# Clone writer-files
+echo "Cloning writer-files locally"
+CLONE_URL=$(echo "https://github.com/$GITHUB_PATH" | sed "s|://|://${USERNAME}:${PAT}@|")
+git clone -b "$GITHUB_BRANCH" "$CLONE_URL" "$TEMP_DIR/writer" --depth 1 --quiet || { echo "Failed to clone repository"; exit 1; }
+
+# Move mappings into github dir
+mv "$CONFIG_TARGET" "$TEMP_DIR/writer"
 
 # Run the Python script with the base paths
 echo "Processing writer files (unzipping, copying files, etc)"
